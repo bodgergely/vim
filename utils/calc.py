dac376e0e086a84ab7ce6ee2ebc61a92152fb5bc
Greg Bod <gergely.bod@hp.com>
2025-12-05 15:40:51 +0100

    Utilities, code generators etc

--- utils/calc.py
+++ utils/calc.py
@@ -3,7 +3,9 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        print(eval(" ".join(sys.argv[1:])))
+        command = " ".join(sys.argv[1:])
+        print(f"[DEBUG] - command:###{command}###EOF")
+        print(eval(command))
     else:
         print(eval(" ".join([line for line in fileinput.input()])))
 
