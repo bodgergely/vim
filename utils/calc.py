6cb82a057a05d52c7c5a0e3f5c90471ec1d4bfc4
Gergely Bod <bodgergely@hotmail.com>
2025-12-27 16:37:34 +0100

    remove debugging line from calc.py

--- utils/calc.py
+++ utils/calc.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         command = " ".join(sys.argv[1:])
-        print(f"[DEBUG] - command:###{command}###EOF")
+        # print(f"[DEBUG] - command:###{command}###EOF")
         print(eval(command))
     else:
         print(eval(" ".join([line for line in fileinput.input()])))
